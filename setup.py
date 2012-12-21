b508d392d17b87d77eede212e4d4b59933ab0a12
Stephan Richter <stephan.richter@gmail.com>
2012-12-21 06:59:58 +0000

    Clean version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup (
     name='z3c.rml',
-    version='2.0.0dev',
+    version='2.0.0',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "An alternative implementation of RML",
