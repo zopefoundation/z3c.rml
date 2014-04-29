f4406c64d4e331a6e53ece8074a9312a11de40c9
Stefan Hudac <stefan@datax.biz>
2014-04-29 20:50:30 +0200

    Fixes issue #10

--- src/z3c/rml/special.py
+++ src/z3c/rml/special.py
@@ -79,7 +79,7 @@ class TextFlowables(object):
         )
 
     def evalString(self, elem, canvas):
-        return do_eval(self._getText(elem, canvas))
+        return do_eval(self._getText(elem, canvas, False))
 
     def namedString(self, elem, canvas):
         self._getManager().names[elem.get('id')] = self._getText(
