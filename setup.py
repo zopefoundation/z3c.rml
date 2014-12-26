9895c7ed82a4de4df759dc6d7a1c5fa23c45786c
Marius Gedminas <marius@gedmin.as>
2014-12-26 14:50:44 +0200

    Reportlab >= 3.0 requires Python 2.7 (or >= 3.3)

--- setup.py
+++ setup.py
@@ -60,7 +60,6 @@ setup (
         'License :: OSI Approved :: Zope Public License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Natural Language :: English',
