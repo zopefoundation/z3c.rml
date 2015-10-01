5ad642a12dcf8b78d8582ef76cdadf868db762b4
fvicente <fvicente@mwneo.com>
2015-09-30 23:19:57 -0300

    Accept file-like objects

--- src/z3c/rml/rml2pdf.py
+++ src/z3c/rml/rml2pdf.py
@@ -44,7 +44,12 @@ def go(xmlInputName, outputFileName=None, outDir=None, dtdDir=None):
     if dtdDir is not None:
         sys.stderr.write('The ``dtdDir`` option is not yet supported.')
 
-    xmlFile = open(xmlInputName, 'r')
+    if hasattr(xmlInputName, 'read'):
+        # it is already a file-like object
+        xmlFile = xmlInputName
+        xmlInputName = 'input.pdf'
+    else:
+        xmlFile = open(xmlInputName, 'r')
     root = etree.parse(xmlFile).getroot()
     doc = document.Document(root)
     doc.filename = xmlInputName
@@ -53,9 +58,14 @@ def go(xmlInputName, outputFileName=None, outDir=None, dtdDir=None):
 
     # If an output filename is specified, create an output filepointer for it
     if outputFileName is not None:
-        if outDir is not None:
-            outputFileName = os.path.join(outDir, outputFileName)
-        outputFile = open(outputFileName, 'wb')
+        if hasattr(outputFileName, 'write'):
+            # it is already a file-like object
+            outputFile = outputFileName
+            outputFileName = 'output.pdf'
+        else:
+            if outDir is not None:
+                outputFileName = os.path.join(outDir, outputFileName)
+            outputFile = open(outputFileName, 'wb')
 
     # Create a Reportlab canvas by processing the document
     doc.process(outputFile)
