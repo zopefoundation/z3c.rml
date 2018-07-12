09ea64ac1edea6fca628172baaf14eb867f05b48
Kyle MacFarlane <kyle@deletethetrees.com>
2018-07-12 18:57:40 +0100

    Fix lists for Reportlab 3.5

--- src/z3c/rml/rlfix.py
+++ src/z3c/rml/rlfix.py
@@ -76,92 +76,208 @@ _type2formatter.update({
 from reportlab.platypus.flowables import ListFlowable, LIIndenter, _LIParams, \
     _computeBulletWidth
 
-def ListFlowable_getContent(self):
-    value = self._start
-    bt = self._bulletType
-    # FIX TO ALLOW ALL FORMATTERS!!!
-    inc = int(bt in _type2formatter.keys())
-    if inc: value = int(value)
+if hasattr(ListFlowable, '_numberStyles'):
+    # Reportlab >= 3.5.0
+    ListFlowable._numberStyles += ''.join(_type2formatter.keys())
 
-    bd = self._bulletDedent
-    if bd=='auto':
-        align = self._bulletAlign
-        dir = self._bulletDir
-        if dir=='ltr' and align=='left':
-            bd = self._leftIndent
-        elif align=='right':
-            bd = self._rightIndent
+    def ListFlowable_getContent(self):
+        bt = self._bulletType
+        value = self._start
+        if isinstance(value,(list,tuple)):
+            values = value
+            value = values[0]
         else:
-            #we need to work out the maximum width of any of the labels
-            tvalue = value
-            maxW = 0
-            for d,f in self._flowablesIter():
-                if d:
-                    maxW = max(maxW,_computeBulletWidth(self,tvalue))
-                    if inc: tvalue += inc
-                elif isinstance(f,LIIndenter):
-                    b = f._bullet
-                    if b:
-                        if b.bulletType==bt:
-                            maxW = max(maxW,_computeBulletWidth(b,b.value))
-                            tvalue = int(b.value)
-                    else:
+            values = [value]
+        autov = values[0]
+        # FIX TO ALLOW ALL FORMATTERS!!!
+        inc = int(bt in _type2formatter.keys())
+        if inc:
+            try:
+                value = int(value)
+            except:
+                value = 1
+
+        bd = self._bulletDedent
+        if bd=='auto':
+            align = self._bulletAlign
+            dir = self._bulletDir
+            if dir=='ltr' and align=='left':
+                bd = self._leftIndent
+            elif align=='right':
+                bd = self._rightIndent
+            else:
+                #we need to work out the maximum width of any of the labels
+                tvalue = value
+                maxW = 0
+                for d,f in self._flowablesIter():
+                    if d:
                         maxW = max(maxW,_computeBulletWidth(self,tvalue))
-                    if inc: tvalue += inc
-            if dir=='ltr':
-                if align=='right':
-                    bd = self._leftIndent - maxW
+                        if inc: tvalue += inc
+                    elif isinstance(f,LIIndenter):
+                        b = f._bullet
+                        if b:
+                            if b.bulletType==bt:
+                                maxW = max(maxW,_computeBulletWidth(b,b.value))
+                                tvalue = int(b.value)
+                        else:
+                            maxW = max(maxW,_computeBulletWidth(self,tvalue))
+                        if inc: tvalue += inc
+                if dir=='ltr':
+                    if align=='right':
+                        bd = self._leftIndent - maxW
+                    else:
+                        bd = self._leftIndent - maxW*0.5
+                elif align=='left':
+                    bd = self._rightIndent - maxW
                 else:
-                    bd = self._leftIndent - maxW*0.5
-            elif align=='left':
-                bd = self._rightIndent - maxW
-            else:
-                bd = self._rightIndent - maxW*0.5
+                    bd = self._rightIndent - maxW*0.5
 
-    self._calcBulletDedent = bd
+        self._calcBulletDedent = bd
 
-    S = []
-    aS = S.append
-    i=0
-    for d,f in self._flowablesIter():
-        fparams = {}
-        if not i:
-            i += 1
-            spaceBefore = getattr(self,'spaceBefore',None)
-            if spaceBefore is not None:
-                fparams['spaceBefore'] = spaceBefore
-        if d:
-            aS(self._makeLIIndenter(f,bullet=self._makeBullet(value),params=fparams))
-            if inc: value += inc
-        elif isinstance(f,LIIndenter):
-            b = f._bullet
-            if b:
-                if b.bulletType!=bt:
-                    raise ValueError('Included LIIndenter bulletType=%s != OrderedList bulletType=%s' % (b.bulletType,bt))
-                value = int(b.value)
-            else:
-                f._bullet = self._makeBullet(value,params=getattr(f,'params',None))
-            if fparams:
-                f.__dict__['spaceBefore'] = max(f.__dict__.get('spaceBefore',0),spaceBefore)
-            aS(f)
-            if inc: value += inc
-        elif isinstance(f,_LIParams):
-            fparams.update(f.params)
-            z = self._makeLIIndenter(f.flowable,bullet=None,params=fparams)
-            if f.first:
-                if f.value is not None:
-                    value = f.value
-                    if inc: value = int(value)
-                z._bullet = self._makeBullet(value,f.params)
+        S = []
+        aS = S.append
+        i=0
+        for d,f in self._flowablesIter():
+            if isinstance(f,ListFlowable):
+                fstart = f._start
+                if isinstance(fstart,(list,tuple)):
+                    fstart = fstart[0]
+                if fstart in values:
+                    #my kind of ListFlowable
+                    if f._auto:
+                        autov = values.index(autov)+1
+                        f._start = values[autov:]+values[:autov]
+                        autov = f._start[0]
+                        if inc: f._bulletType = autov
+                    else:
+                        autov = fstart
+            fparams = {}
+            if not i:
+                i += 1
+                spaceBefore = getattr(self,'spaceBefore',None)
+                if spaceBefore is not None:
+                    fparams['spaceBefore'] = spaceBefore
+            if d:
+                aS(self._makeLIIndenter(f,bullet=self._makeBullet(value),params=fparams))
                 if inc: value += inc
-            aS(z)
-        else:
-            aS(self._makeLIIndenter(f,bullet=None,params=fparams))
+            elif isinstance(f,LIIndenter):
+                b = f._bullet
+                if b:
+                    if b.bulletType!=bt:
+                        raise ValueError('Included LIIndenter bulletType=%s != OrderedList bulletType=%s' % (b.bulletType,bt))
+                    value = int(b.value)
+                else:
+                    f._bullet = self._makeBullet(value,params=getattr(f,'params',None))
+                if fparams:
+                    f.__dict__['spaceBefore'] = max(f.__dict__.get('spaceBefore',0),spaceBefore)
+                aS(f)
+                if inc: value += inc
+            elif isinstance(f,_LIParams):
+                fparams.update(f.params)
+                z = self._makeLIIndenter(f.flowable,bullet=None,params=fparams)
+                if f.first:
+                    if f.value is not None:
+                        value = f.value
+                        if inc: value = int(value)
+                    z._bullet = self._makeBullet(value,f.params)
+                    if inc: value += inc
+                aS(z)
+            else:
+                aS(self._makeLIIndenter(f,bullet=None,params=fparams))
 
-    spaceAfter = getattr(self,'spaceAfter',None)
-    if spaceAfter is not None:
-        f=S[-1]
-        f.__dict__['spaceAfter'] = max(f.__dict__.get('spaceAfter',0),spaceAfter)
-    return S
+        spaceAfter = getattr(self,'spaceAfter',None)
+        if spaceAfter is not None:
+            f=S[-1]
+            f.__dict__['spaceAfter'] = max(f.__dict__.get('spaceAfter',0),spaceAfter)
+        return S
+else:
+    # Reportlab < 3.5.0
+    def ListFlowable_getContent(self):
+        value = self._start
+        bt = self._bulletType
+        # FIX TO ALLOW ALL FORMATTERS!!!
+        inc = int(bt in _type2formatter.keys())
+        if inc: value = int(value)
+
+        bd = self._bulletDedent
+        if bd=='auto':
+            align = self._bulletAlign
+            dir = self._bulletDir
+            if dir=='ltr' and align=='left':
+                bd = self._leftIndent
+            elif align=='right':
+                bd = self._rightIndent
+            else:
+                #we need to work out the maximum width of any of the labels
+                tvalue = value
+                maxW = 0
+                for d,f in self._flowablesIter():
+                    if d:
+                        maxW = max(maxW,_computeBulletWidth(self,tvalue))
+                        if inc: tvalue += inc
+                    elif isinstance(f,LIIndenter):
+                        b = f._bullet
+                        if b:
+                            if b.bulletType==bt:
+                                maxW = max(maxW,_computeBulletWidth(b,b.value))
+                                tvalue = int(b.value)
+                        else:
+                            maxW = max(maxW,_computeBulletWidth(self,tvalue))
+                        if inc: tvalue += inc
+                if dir=='ltr':
+                    if align=='right':
+                        bd = self._leftIndent - maxW
+                    else:
+                        bd = self._leftIndent - maxW*0.5
+                elif align=='left':
+                    bd = self._rightIndent - maxW
+                else:
+                    bd = self._rightIndent - maxW*0.5
+
+        self._calcBulletDedent = bd
+
+        S = []
+        aS = S.append
+        i=0
+        for d,f in self._flowablesIter():
+            fparams = {}
+            if not i:
+                i += 1
+                spaceBefore = getattr(self,'spaceBefore',None)
+                if spaceBefore is not None:
+                    fparams['spaceBefore'] = spaceBefore
+            if d:
+                aS(self._makeLIIndenter(f,bullet=self._makeBullet(value),params=fparams))
+                if inc: value += inc
+            elif isinstance(f,LIIndenter):
+                b = f._bullet
+                if b:
+                    if b.bulletType!=bt:
+                        raise ValueError('Included LIIndenter bulletType=%s != OrderedList bulletType=%s' % (b.bulletType,bt))
+                    value = int(b.value)
+                else:
+                    f._bullet = self._makeBullet(value,params=getattr(f,'params',None))
+                if fparams:
+                    f.__dict__['spaceBefore'] = max(f.__dict__.get('spaceBefore',0),spaceBefore)
+                aS(f)
+                if inc: value += inc
+            elif isinstance(f,_LIParams):
+                fparams.update(f.params)
+                z = self._makeLIIndenter(f.flowable,bullet=None,params=fparams)
+                if f.first:
+                    if f.value is not None:
+                        value = f.value
+                        if inc: value = int(value)
+                    z._bullet = self._makeBullet(value,f.params)
+                    if inc: value += inc
+                aS(z)
+            else:
+                aS(self._makeLIIndenter(f,bullet=None,params=fparams))
+
+        spaceAfter = getattr(self,'spaceAfter',None)
+        if spaceAfter is not None:
+            f=S[-1]
+            f.__dict__['spaceAfter'] = max(f.__dict__.get('spaceAfter',0),spaceAfter)
+        return S
 
 ListFlowable._getContent = ListFlowable_getContent
