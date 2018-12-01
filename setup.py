302fd9dd92beb9c80a4928a74d806afad7af8a8c
Stephan Richter <stephan.richter@gmail.com>
2018-12-01 03:22:16 -0500

    Back to development: 3.6.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ TESTS_REQUIRE = [
 
 setup (
     name="z3c.rml",
-    version='3.6.0',
+    version='3.6.1.dev0',
     author="Stephan Richter and the Zope Community",
     author_email="zope-dev@zope.org",
     description="An alternative implementation of RML",
